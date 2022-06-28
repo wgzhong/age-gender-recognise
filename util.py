41720b089aeabd1595c9a12d5ab790037ffe87c4
Zwglight <871981266@qq.com>
2022-06-28 11:24:56 -0400

    fix acc

--- util.py
+++ util.py
@@ -4,17 +4,21 @@ from tensorflow import keras
 class BinaryFocalLossAccuracy(keras.metrics.Metric):
     def __init__(self, name='BinaryFocalLossAccuracy', **kwargs):
         super(BinaryFocalLossAccuracy, self).__init__(name=name, **kwargs)
-        self.tp = self.add_weight(name='tp', initializer='zeros')
+        self.tp = self.add_weight(name='tp', dtype=tf.int32, initializer=tf.zeros_initializer())
+        self.total = self.add_weight(name='total', dtype=tf.int32, initializer=tf.zeros_initializer())
     def update_state(self, y_true, y_pred):
         gender = tf.where(y_pred[:,0]>0.5, 1, 0)
         p_age = tf.argmax(y_true[0:,1:], axis=-1, output_type=tf.int32)
         age = tf.argmax(y_pred[0:,1:], axis=-1, output_type=tf.int32)
         a1 = tf.equal(gender, y_true[:,0])
         a2 = tf.equal(age, p_age)
-        self.tp.assign_add(tf.reduce_mean(tf.cast(a1&a2, dtype='float32')))
+        a3 = tf.cast(a1&a2, dtype="int32")
+        # self.tp.assign_add(tf.reduce_mean(tf.cast(a1&a2, dtype='float32')))
+        self.total.assign_add(y_true.shape[0])
+        self.tp.assign_add(tf.reduce_sum(a3))
  
     def result(self):
-        return self.tp
+        return self.tp / self.total
 
 
 
